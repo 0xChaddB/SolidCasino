13c7bad07e53e013987202b6e3d7dea69dc60c2e
Samuel <chaddb@mac.home>
2025-05-28 14:28:46 +0200

    first commit

---
+++ src/Blackjack.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.3.0/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts@1.3.0/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+contract Blackjack is VRFConsumerBaseV2Plus {
+    
+}
