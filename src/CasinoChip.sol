a22620e56657a9b8cc1e596d690cef9682b2385c
Chaddb <0xchaddb@proton.me>
2025-06-04 11:44:53 +0200

    feat: mint & burn & access control

--- src/CasinoChip.sol
+++ src/CasinoChip.sol
@@ -1,6 +1,25 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-contract CasinoChip {
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
+contract CasinoChip is ERC20, AccessControl {
+
+    bytes32 public constant CASINO_ROLE = keccak256("CASINO_ROLE");
+
+    address public immutable casinoBank;
+
+    constructor() ERC20("Casino Chip", "CHIP") {
+        _grantRole(CASINO_ROLE, casinoBank);
+        _grantRole(CASINO_ROLE, msg.sender); // governance 
+    }
+
+    function mint(address to, uint256 amount) external onlyRole(CASINO_ROLE) {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyRole(CASINO_ROLE) {
+        _burn(from, amount);
+    }
 }
