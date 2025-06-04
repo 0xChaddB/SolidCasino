da7c5c9dc01c4442b6682b196591010ac7af7763
Chaddb <0xchaddb@proton.me>
2025-06-04 11:20:50 +0200

    comments

--- src/CasinoBank.sol
+++ src/CasinoBank.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import {CasinoChip} from "./CasinoChip.sol";
 
@@ -9,24 +8,34 @@ contract CasinoBank {
 
     CasinoChip public immutable casinoChip;
 
+    //////////////////////////
+    //        EVENTS        //
+    //////////////////////////
+
+    event CasinoBank__Deposit(address indexed from, address indexed token, uint256 indexed amount);
+
     // user => token => balance
     mapping(address => mapping(address => uint256)) public userBalances;
     
     mapping(address => bool) public whitelistedTokens;
     address[] public allTokens;
 
+    //////////////////////////
+    //      CONSTRUCTOR     //
+    //////////////////////////
+
     constructor(address _chip) {
         casinoChip = CasinoChip(_chip);
     }
 
-    // I may need to check the depositor address....
+    // I may need to check the depositor address.... someone maybe could inflate their balance 
     function depositTokens(uint256 amount, address token) external payable returns (bool) {
         if (token == address(0)) {
             // ETH deposit
             require(msg.value > 0, "No ETH sent");
             require(amount == msg.value, "Amount mismatch");
             userBalances[msg.sender][address(0)] += msg.value;
-            return true;
+            return true; // event instead of return ? 
         } else {
             // ERC20 deposit
             require(msg.value == 0, "Don't send ETH with token");
@@ -36,7 +45,7 @@ contract CasinoBank {
             bool success = ERC20(token).transferFrom(msg.sender, address(this), amount);
             require(success, "Transfer failed");
             userBalances[msg.sender][token] += amount;
-            return success;
+            return true; // event instead of return ? 
         }
     }
 
