29765ae22d8470a237bcaabd9e5eaa72b4adb1e4
Chaddb <0xchaddb@proton.me>
2025-06-04 10:55:32 +0200

    refactor: cleaned vault casino

--- src/CasinoBank.sol
+++ src/CasinoBank.sol
@@ -2,17 +2,18 @@
 pragma solidity ^0.8.26;
 
 
-import {ERC4626, IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import {CasinoChip} from "./CasinoChip.sol";
 
-contract CasinoBank is ERC4626 {
+contract CasinoBank {
 
-    CasinoToken public immutable casinoToken;
+    CasinoChip public immutable casinoChip;
 
-    constructor(address _token) {
-        casinoToken = CasinoToken(_token);
+    constructor(address _chip) {
+        casinoChip = CasinoChip(_chip);
     }
 
-    
+
+
+
 }
